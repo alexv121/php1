2bcc6911d8b4d9fc24642915d4b6c28cde0cbcb5
Alexandra Vaughn <alexvaughn121@gmail.com>
2018-11-08 16:09:24 -0500

    more variables

--- hello.php
+++ hello.php
@@ -6,4 +6,12 @@ $y = 2;
 $sum = $x + $y;
 echo $sum;
 
+$name = "Alex";
+$age = 26;
+echo "Hello, $name.You are $age years old.\n";
+$x = 195793;
+$y = 256836;
+$sum = $x + $y;
+
+echo "The sum of $x and $y is $sum."
 ?>
